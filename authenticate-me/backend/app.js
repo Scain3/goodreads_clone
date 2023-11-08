ea0e0eb5e475b418d9a330023ab5f534b547e32c
Safiya Cain <64106105+Scain3@users.noreply.github.com>
2023-11-08 09:56:29 -0500

    add csrf routes

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const { csrfSync } = require("csrf-sync");
+
+const {
+    invalidCsrfTokenError, // This is just for convenience if you plan on making your own middleware.
+    generateToken, // Use this in your routes to generate, store, and get a CSRF token.
+    getTokenFromRequest, // use this to retrieve the token submitted by a user
+    getTokenFromState, // The default method for retrieving a token from state.
+    storeTokenInState, // The default method for storing a token in state.
+    revokeToken, // Revokes/deletes a token by calling storeTokenInState(undefined)
+    csrfSynchronisedProtection, // This is the default CSRF protection middleware.
+  } = csrfSync();
+
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
@@ -14,6 +25,22 @@ app.use(morgan('dev'));
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(session);
+app.get("/csrf-token", myRoute);
+app.use(csrfSynchronisedProtection);
+
+const myCsrfProtectionMiddleware = (req, res, next) => {
+    // Some method to determine whether we want CSRF protection to apply
+    if (isCsrfProtectionNeeded(req)) {
+      // protect with CSRF
+      csrfSynchronisedProtection(req, res, next);
+    } else {
+      // Don't protect with CSRF
+      next();
+    }
+  };
+  app.use(myCsrfProtectionMiddleware);
 
 // Security Middleware
 if (!isProduction) {
--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/hello/world', function(req, res) {
-//   res.send('Hello World!');
-// });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
-router.get('/hello/world', (req, res)=> {res.send('Hello World!')});
+// router.get('/hello/world', (req, res)=> {res.send('Hello World!')});
 
 exports = router;
